42034a900cfde4761914236f7d54386a2c6ace1e
remoulade96 <daniella.duverne@gmail.com>
2026-04-15 12:36:04 -0500

    Update Bucket.java

--- Bucket.java
+++ Bucket.java
@@ -4,10 +4,12 @@ public class Bucket {
 	String key;
 	Object value;
 	int ticket;
+	String status;
 	
 	Bucket(String key, Object value){
 		this.key = key;
 		this.value = value;
-
+		this.status = "Pending";
+		
 	}
 }
